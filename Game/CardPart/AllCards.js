efef263c645866a35742e8de48e14c83079aae91
Ziga <ziga.v.r@gmail.com>
2018-04-21 16:09:04 +0200

    AllCards Added as Object

--- Game/CardPart/AllCards.js
+++ Game/CardPart/AllCards.js
@@ -1,8 +1,17 @@
 class AllCards{
     static get Cards(){
         return {
-            'idcart': {health:4, damage:4, type:'Head', asset:'',  attachPoint:{x:0,y:30}},
-            
+            'head1': {health:1, damage:4, usedBy:'Character:Enemy', type:'Head', asset:'Assets/CardParts/Heads/head1.png',  attachPoint:{x:0,y:30}},
+            'head2': {health:2, damage:5, usedBy:'Character', type:'Head', asset:'Assets/CardParts/Heads/head2.png',  attachPoint:{x:0,y:0}},
+            'head3': {health:3, damage:6, usedBy:'Character', type:'Head', asset:'Assets/CardParts/Heads/head3.png',  attachPoint:{x:0,y:0}},
+            'body1': {health:2, damage:1, usedBy:'Character:Enemy', type:'Body', asset:'Assets/CardParts/Bodys/body1.png',  attachPoint:{x:0,y:0}},
+            'body2': {health:3, damage:2, usedBy:'Character', type:'Body', asset:'Assets/CardParts/Bodys/body2.png',  attachPoint:{x:0,y:0}},
+            'body3': {health:4, damage:3, usedBy:'Character', type:'Body', asset:'Assets/CardParts/Bodys/body3.png',  attachPoint:{x:0,y:0}},
+            'leg1': {health:7, damage:1, usedBy:'Character:Enemy', type:'Leg', asset:'Assets/CardParts/Legs/leg1.png',  attachPoint:{x:0,y:0}},
+            'leg2': {health:8, damage:2, usedBy:'Character', type:'Leg', asset:'Assets/CardParts/Legs/leg2.png',  attachPoint:{x:0,y:0}},
+            'leg3': {health:9, damage:3, usedBy:'Character', type:'Leg', asset:'Assets/CardParts/Legs/leg3.png',  attachPoint:{x:0,y:0}},
+            'tail1': {health:4, damage:1, usedBy:'Character:Enemy', type:'Tail', asset:'Assets/CardParts/Tails/tail1.png',  attachPoint:{x:0,y:0}},
+            'tail2': {health:5, damage:2, usedBy:'Character', type:'Tail', asset:'Assets/CardParts/Tails/tail2.png',  attachPoint:{x:0,y:0}},
         }
     }
 }
